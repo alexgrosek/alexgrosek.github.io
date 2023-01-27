48bf30fe0caed59f683f92051ce52bedf70b5ec8
alexgrosek <agrosek1@gmail.com>
2023-01-27 01:18:57 -0500

    First Commit

--- sketch.js
+++ sketch.js
@@ -1,98 +1,40 @@
-let points = 0
-let pointcheck = true;
-let r = 0;
+let xlist = [];
+let ylist = [];
+let ops = [-3,3];
 
-
+// comment
 function setup() {
-  createCanvas(500,500);
-  textAlign(LEFT);
-  angleMode(RADIANS);
-  frameRate(60);
+  createCanvas(775,400);
+  for (let i = 0; i < 400; i+=60){
+    values(i+15);
+  }
 }
-
 
 function draw() {
-  let c = [mouseX,mouseY];
-  let d = [width/2,height/2];
-  
-  background(220);
-  noFill();
-  ellipse(width/2,height/2,30);
-  ellipse(mouseX,mouseY,12,12);
-  
-
-  push();
-  translate(width/2,height/2);
-  rotate(r=r+.01);
-  arc1 = arc(0,0,80,80,PI,TWO_PI);
-  //rotate(r=r+.01);
-  //arc2 = arc(0,0,150,150,PI,QUARTER_PI);
-  console.log(lineIntersectsArc([width/2,height/2],[mouseX,mouseY],[width/2,height/2],80,0,0))
-  pop();
-  
-  let e = [0,0]
-  let f = [PI,TWO_PI]
-  
-  line(width/2,height/2,mouseX,mouseY);
-  textSize(14);
-  fill(255,0,0);
-  
-  text(points,50,50);
-  
-  if (points >=3){
-    text('GAME OVER',50,200);  
-    noLoop();
-  }
-  
-  if(dist(width/2,height/2,mouseX,mouseY)<10 && pointcheck == true){
-    points = points + 1;
-    pointcheck = false;
-  }
-  
-  if(dist(width/2,height/2,mouseX,mouseY)>10){
-    pointcheck=true;
-  }
+  //background(220);
+  drawBlack();
+  xlist = [];
+  ylist = [];
 }
 
-function lineIntersectsArc(lineStart, lineEnd, arcCenter, arcRadius, arcStart, arcEnd) {
-  // Find the distance between the line's start and end points
-  var lineDelta = {
-    x: lineEnd.x - lineStart.x,
-    y: lineEnd.y - lineStart.y
-  };
+function values(z){
+  for (let i = 0; i < 800; i+=8) {
+    x = random()*random(ops) + i
+    y = random()*random(ops) + z
+    append(xlist,x)
+    append(ylist,y)
+  redraw()
+}
+}
 
-  // Find the distance between the arc's center and the line
-  var centerToLineStart = {
-    x: lineStart.x - arcCenter.x,
-    y: lineStart.y - arcCenter.y
-  };
-
-  // Calculate the dot product of the centerToLineStart vector and the lineDelta vector
-  var dotProduct = centerToLineStart.x * lineDelta.x + centerToLineStart.y * lineDelta.y;
-
-  // Calculate the squared length of the lineDelta vector
-  var squaredLineLength = lineDelta.x * lineDelta.x + lineDelta.y * lineDelta.y;
-
-  // Find the closest point on the line to the center of the arc
-  var closestPoint = {
-    x: lineStart.x + dotProduct * lineDelta.x / squaredLineLength,
-    y: lineStart.y + dotProduct * lineDelta.y / squaredLineLength
-  };
-
-  // Check if the closest point is within the arc's radius
-  if (dist(arcCenter.x, arcCenter.y, closestPoint.x, closestPoint.y) <= arcRadius) {
-    // Check if the closest point is on the line segment
-    if (closestPoint.x >= min(lineStart.x, lineEnd.x) && closestPoint.x <= max(lineStart.x, lineEnd.x) &&
-        closestPoint.y >= min(lineStart.y, lineEnd.y) && closestPoint.y <= max(lineStart.y, lineEnd.y)) {
-      // Check if the closest point is within the arc's start and end angles
-      var angleToClosestPoint = atan2(closestPoint.y - arcCenter.y, closestPoint.x - arcCenter.x);
-      if (angleToClosestPoint < 0) {
-        angleToClosestPoint += TWO_PI;
-      }
-      if (angleToClosestPoint >= arcStart && angleToClosestPoint <= arcEnd) {
-        return true;
-      }
-    }
-  }
-  return false;
+function drawBlack(){
+  for (let i = 0; i < 800; i++) {
+      strokeWeight(3);
+      beginShape();
+      curveVertex(xlist[i],ylist[i])
+      curveVertex(xlist[i],ylist[i])
+      curveVertex(xlist[i+1],ylist[i+1])
+      curveVertex(xlist[i+1],ylist[i+1])
+      endShape(CLOSE);
+}
 }
