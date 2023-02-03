a931194611b0d49c85c09b30eda6d995c2a3170b
alexgrosek <agrosek1@gmail.com>
2023-02-03 12:32:18 -0500

    Brave New World

--- sketch.js
+++ sketch.js
@@ -1,63 +1,61 @@
+//import { FlyControls } from "./three/FlyControls";
+
 let r = 0;
 let x = 0;
 let y = 0;
 let j = 0;
+let z = 0;
+let m = 0;
 
 function setup() {
-  //let img = createImg("https://toppng.com/uploads/preview/north-arrow-11549436420yyvgvtd8b7.png");
-  let img = createImg()
-  let div = createDiv("Look Around")
+  let img = createImg("https://media.giphy.com/media/GPWKoHhTMmjTYqOTVG/giphy.gif");
+  //let img = createImg();
+    img.position(1000,100);
+    img.size(150,150);
+  
+  // let div = createDiv("Look Around");
+  //   div.position(1000, 450);
+  //   div.size(200, 200);
 
-  let canvas = createCanvas(windowWidth,windowHeight)
-  canvas.parent("p5js_canvas")
-  //angleMode(RADIANS);
-  //normalMaterial();
-
-  div.position(1000, 450);
-  div.size(200, 200);
-
-  // img.position(50,50);
-  // img.size(100,100);
+  let canvas = createCanvas(windowWidth,windowHeight);
+  canvas.parent("p5js_canvas");
   canvas.position(0,0);
+
   // This is the inflection point, where you can EITHER have orbit control and div element or sketch.js and div element but for some reason you can't have all three....
-  //orbitControl();
   controls.update();
+  //flycontrols.update();
+  //orbitControls.update();
 }
 
 function draw() {
-  // noStroke();
-  background(220, 180, 200,0);
-  // fill(180, 200, 40);
-  // strokeWeight(6);
-  // stroke(180, 100, 240);
-  // for (let i = 0; i < width; i += 15) {
-  //   line(i, 0, i, height);
-  // }
-  
-  // different script
 
-  //background(220,220,220)
-  push();
-  ellipse(width/3,height/3,10);
-  translate(width/3,height/3)
-  rotate(r=r+100);
-  fill('red')
-  ellipse(10,100,10);
-  pop();
+  background(220, 180, 200,0);
+
+  // ellipse(width/3,height/3,10);
+  // translate(width/3,height/3)
+  // rotate(r=r+.05);
+  // ellipse(20,20,10);
+
+  // push();
+  // ellipse(width/3,height/3,10);
+  // translate(width/3,height/3)
+  // rotate(r=r+100);
+  // fill('red')
+  // ellipse(10,100,10);
+  // pop();
 
   // new script
 
-  push();
-  noStroke();
-  translate(400,400);
-  j=j+1;
-  y=sin(r=r+0.1) * j;
-  x=cos(r=r+0.1) * j;
-  stroke(0);
-  noFill(0,250,120);
-  ellipse(x,y++,14);
-  pop();
+  // push();
+  // noStroke();
+  // translate(400,400);
+  // j=j+1;
+  // y=sin(r=r+0.1) * j;
+  // x=cos(r=r+0.1) * j;
+  // stroke(0);
+  // noFill(0,250,120);
+  // ellipse(x,y++,14);
+  // pop();
 
-  // orbitControl();
-  // controls.update();
+ 
 }
--- threeD.js
+++ threeD.js
@@ -1,18 +1,27 @@
 // Import the necessary libraries for orbiting and loading 3D models
 
-import {OrbitControls} from "../three/OrbitControls.js";
-import {GLTFLoader} from "../three/GLTFLoader.js";
+
+
+//import { SkeletonUtils } from 'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/jsm/utils/SkeletonUtils.js';
+import { OrbitControls } from "../three/OrbitControls.js";
+import { GLTFLoader } from "../three/GLTFLoader.js";
+import { FlyControls } from "../three/FlyControls.js";
+import { clone } from "../three/SkeletonUtils.js";
 
 // Create a new instance of the GLTFloader, camera, and three.js scene with optional camera position and background settings
 var loader = new GLTFLoader();
 
 var scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x000fff);
+    //scene.background = new THREE.Color(0x000fff);
 
-var camera = new THREE.PerspectiveCamera(6, window.innerWidth / window.innerHeight, .01, 10000 );
-    camera.position.set(-100,60,75);
-    //camera.lookAt(new THREE.Vector3(-200,0,0))
-    // camera.lookAt(-200,1000,50)
+const textureloader = new THREE.TextureLoader();
+textureloader.load('https://images.pexels.com/photos/1205301/pexels-photo-1205301.jpeg' , function(texture)
+    {
+        scene.background = texture;  
+    });
+
+var camera = new THREE.PerspectiveCamera(4.2, window.innerWidth / window.innerHeight, .01, 10000 );
+    camera.position.set(-100,45,100);
     console.log(camera.position);
 
 var renderer = new THREE.WebGLRenderer( { alpha: true } );
@@ -20,172 +29,107 @@ var renderer = new THREE.WebGLRenderer( { alpha: true } );
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls(camera, renderer.domElement);
+// var flycontrols = new THREE.FlyControls( camera, render.domElement );
+//     // flycontrols.movementSpeed = 100;
+//     // flycontrols.rollSpeed = Math.PI / 24;
+//     // flycontrols.autoForward = false;
+//     // flycontrols.dragToLook = false;
+//     flycontrols.update();
+
+var controls = new OrbitControls(camera, renderer.domElement);
     controls.update();
 
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-    directionalLight.position.set(0,1,0);
-    scene.add( directionalLight );
-
-var obj;
-loader.load("City_1.glb",function(gltf){
-  obj = gltf.scene;
-  var newMaterial = new THREE.MeshPhongMaterial({color: 0xffffff, wireframe: false}); // set object color
-  var light1 = new THREE.PointLight( 0xff0040,.1,50 ); // light color, intensity and position
-  var light2 = new THREE.PointLight( 0x0040ff,.1,50 ); // light color, intensity and position
-  var light3 = new THREE.PointLight( 0xff80ff,.1,50 ); // light color, intensity and position
-  scene.add( light1,light2,light3 ); // turn on point lights
-  obj.traverse((o) => {
-    if (o.isMesh || o.isLight) {o.castShadow = true, o.material = newMaterial;} // 
-  });      
-  gltf.scene.castShadow = true;
-  gltf.scene.recieveShadow = true;
-  gltf.scene.position.x = -5;
-  gltf.scene.position.y = 0;
-  scene.add(gltf.scene);
-});
-
-var obj2;
-loader.load("City_2.glb",function(gltf){
-  obj2 = gltf.scene;
-  var newMaterial = new THREE.MeshPhongMaterial({color: 0xffffff, wireframe: false}); // set object color
-  var light1 = new THREE.PointLight( 0xff0040,.1,50 ); // light color, intensity and position
-  var light2 = new THREE.PointLight( 0x0040ff,.1,50 ); // light color, intensity and position
-  var light3 = new THREE.PointLight( 0xff80ff,.1,50 ); // light color, intensity and position
-  scene.add( light1,light2,light3 ); // turn on point lights
-  obj2.traverse((o) => {
-    if (o.isMesh || o.isLight) {o.castShadow = true, o.material = newMaterial;} // 
-  });      
-  gltf.scene.castShadow = true;
-  gltf.scene.recieveShadow = true;
-  gltf.scene.position.x = -9;
-  gltf.scene.position.y = 0;
-  scene.add(gltf.scene);
-});
-
-var obj3;
-loader.load("City_3.glb",function(gltf){
-  obj3 = gltf.scene;
-  var newMaterial = new THREE.MeshPhongMaterial({color: 0xffffff, wireframe: false}); // set object color
-  var light1 = new THREE.PointLight( 0xff0040,.1,50 ); // light color, intensity and position
-  var light2 = new THREE.PointLight( 0x0040ff,.1,50 ); // light color, intensity and position
-  var light3 = new THREE.PointLight( 0xff80ff,.1,50 ); // light color, intensity and position
-  scene.add( light1,light2,light3 ); // turn on point lights
-  obj3.traverse((o) => {
-    if (o.isMesh || o.isLight) {o.castShadow = true, o.material = newMaterial;} // 
-  });      
-  gltf.scene.castShadow = true;
-  gltf.scene.recieveShadow = true;
-  gltf.scene.position.x = -13;
-  gltf.scene.position.y = 0;
-  gltf.scene.position.z = -2;
-
-  scene.add(gltf.scene);
-});
-
-var obj3;
-loader.load("City_3.glb",function(gltf){
-  obj3 = gltf.scene;
-  var newMaterial = new THREE.MeshPhongMaterial({color: 0xffffff, wireframe: false}); // set object color
-  var light1 = new THREE.PointLight( 0xff0040,.1,0); // light color, intensity and position
-  var light2 = new THREE.PointLight( 0x0040ff,.1,0 ); // light color, intensity and position
-  var light3 = new THREE.PointLight( 0xff80ff,.1,0 ); // light color, intensity and position
-  scene.add( light1,light2,light3 ); // turn on point lights
-  obj3.traverse((o) => {
-    if (o.isMesh || o.isLight) {o.castShadow = true, o.material = newMaterial;} // 
-  });      
-  gltf.scene.castShadow = true;
-  gltf.scene.recieveShadow = true;
-  gltf.scene.position.x = -5;
-  gltf.scene.position.y = 0;
-  gltf.scene.position.z = 7;
-
-  scene.add(gltf.scene);
-});
-
-var obj4;
-loader.load("City_4.glb",function(gltf){
-  obj4 = gltf.scene;
-  var newMaterial = new THREE.MeshPhongMaterial({color: 0xffffff, wireframe: false}); // set object color
-  var light1 = new THREE.PointLight( 0xff0040,.1,50 ); // light color, intensity and position
-  var light2 = new THREE.PointLight( 0x0040ff,.1,50 ); // light color, intensity and position
-  var light3 = new THREE.PointLight( 0xff80ff,.1,50 ); // light color, intensity and position
-  scene.add( light1,light2,light3 ); // turn on point lights
-  obj4.traverse((o) => {
-    if (o.isMesh || o.isLight) {o.castShadow = true, o.material = newMaterial;} // 
-  });      
-  gltf.scene.castShadow = true;
-  gltf.scene.recieveShadow = true;
-  gltf.scene.position.x = -10;
-  gltf.scene.position.y = 0;
-  gltf.scene.position.z = -7;
-
-  scene.add(gltf.scene);
-});
-
-function addLight(){
-    var ambientLight = new THREE.AmbientLight( 0xcccccc );
+var ambientLight = new THREE.AmbientLight( 0xcccccc,.17 );
     scene.add( ambientLight );
-  };
+
+// var directionalLight = new THREE.DirectionalLight( 0xff0040, .1,0 );
+//     directionalLight.position.set(-1,-1,0);
+//     scene.add( directionalLight );
+
+// var directionalLight = new THREE.DirectionalLight( 0xff80ff, .1,0 );
+//     //directionalLight.position.set(1,1,0);
+//     scene.add( directionalLight );
+
+var light1 = new THREE.PointLight( 0xff0040,.5,50 ); // light color, intensity and position
+var light2 = new THREE.PointLight( 0x0040ff,.5,50 ); // light color, intensity and position
+var light3 = new THREE.PointLight( 0xff80ff,.5,50 ); // light color, intensity and position
+
+function revolve(){
+  var time = Date.now()*.001;
   
-addLight();
-  
+  light1.position.x = Math.sin( time * 0.7) * 15//30;
+  light1.position.y = Math.cos( time * 0.5) * 20//40;
+  light1.position.z = Math.cos( time * 0.3) * 15//30;
+
+  light2.position.x = Math.cos( time * 0.3) * 15//30;
+  light2.position.y = Math.sin( time * 0.5) * 20//40;
+  light2.position.z = Math.sin( time * 0.7) * 15//30;
+
+  light3.position.x = Math.sin( time * 0.7) * 15//30;
+  light3.position.y = Math.cos( time * 0.3) * 20//40;
+  light3.position.z = Math.sin( time * 0.5) * 15//30;
+}
+
+function loadToScene(model,xval,yval,zval,rval){
+    // Add original object
+    //model.position.set(0,10,0);
+    //scene.add(model);
+
+    // Clone a copy
+    var model2 = model.clone(true);
+    model2.position.set(xval,yval,zval);
+    model2.rotation.y += rval
+    scene.add(model2);
+}
+
+function threeLoad(stag,file,xval,yval,zval,rval,clr,copy) {
+    //var cities = new Array();
+    loader.load(file,function(gltf){
+        const model = gltf.scene;
+        var newMaterial = new THREE.MeshLambertMaterial({color: clr, wireframe: false}); // set object color
+        scene.add( light1,light2,light3 ); // turn on point lights
+        model.traverse((o) => {
+          if (o.isMesh || o.isLight) {o.castShadow = true, o.material = newMaterial;} // 
+        });      
+        model.castShadow = true;
+        model.recieveShadow = true;
+        model.position.x = xval;
+        model.position.y = yval;
+        model.position.z = zval;
+        model.rotation.y += rval;
+        scene.add(model);
+        //cities.push(stag)
+        if (copy == true){
+            loadToScene(model,xval+7,yval,zval-7, Math.PI / 2)
+        }
+      });
+    //loadToScene(cities[0],xval,10,zval)
+}
+
+let obj;
+let obj2;
+let obj3;
+let obj4;
+let obj5;
+
+threeLoad(obj,"City_1.glb",1,-6,0,0,0xffffff,true);
+threeLoad(obj2,"City_2.glb",-3,-6,0,0,0xffffff,true);
+threeLoad(obj3,"City_3.glb",-7,-6,-2,0,0xffffff,true);
+threeLoad(obj4,"City_3.glb",1,-6,7,0,0xffffff,false);
+threeLoad(obj5,"City_4.glb",5,-6,5,0,0xffffff,false);
+
 function animate (){
     requestAnimationFrame(animate);
     renderer.render(scene,camera);
+    //flycontrols.update();
     //obj.rotation.y += .001;
-    //obj2.rotation.x += .001;
-    //obj3.rotation.y += .001;
+    // obj2.rotation.x += .001;
+    // obj3.rotation.y += .001;
       // required if controls.enableDamping or controls.autoRotate are set to true
     //controls.update();
     // draw();
+    // flycontrols.update();
+    revolve();
   }
 
 animate();
-// var obj2;
-// loader.load("City_1.glb",function(gltf){
-//   obj2 = gltf.scene;
-//   gltf.scene.position.x = -2;
-//   gltf.scene.position.y = -7;
-//   scene.add(gltf.scene);
-// });
-
-// var obj3;
-// loader.load("City_1.glb",function(gltf){
-//   obj3 = gltf.scene;
-//   gltf.scene.position.x = -6;
-//   gltf.scene.position.y = -7;
-//   scene.add(gltf.scene);
-// });
-
-//light = new THREE.AmbientLight( 0xffffff ); // soft white light
-//scene.add( light );
-
-
-//hemiLight = new THREE.HemisphereLight(0xffeeb1,0x080820,.5);
-//scene.add(hemiLight)
-
-
-//Add light
-//var light = new THREE.AmbientLight(0x000fff,0.5);
-//scene.add(light);
-
-//let hemiLight;
-//hemiLight = new THREE.HemisphereLight(0xffeeb1,0x080820,.5);
-//scene.add(hemiLight)
-
-// function revolve(){
-//   var time = Date.now() * 1000;
-  
-//   light1.position.x = Math.sin( time * 0.7) * 30;
-//   light1.position.y = Math.cos( time * 0.5) * 40;
-//   light1.position.z = Math.cos( time * 0.3) * 30;
-
-//   light2.position.x = Math.cos( time * 0.3) * 30;
-//   light2.position.y = Math.sin( time * 0.5) * 40;
-//   light2.position.z = Math.sin( time * 0.7) * 30;
-
-//   light3.position.x = Math.sin( time * 0.7) * 30;
-//   light3.position.y = Math.cos( time * 0.3) * 40;
-//   light3.position.z = Math.sin( time * 0.5) * 30;
-// }
